ca4843492f3993a0c441c288d8e4a3e42761d415
Marcelo Choque <casiprogramador@gmail.com>
2016-09-29 01:52:42 -0400

    Terminado crud de contactos

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -78,7 +78,7 @@ class ContactController extends Controller
 		//Subir fotografia
         $id_user = Auth::user()->id;
         $file = $request->file('fotografia');
-        $tmpFilePath = '/img/upload/fotografia_contacto';
+        $tmpFilePath = '/img/upload/fotografia_contacto/';
         $tmpFileName = time() . '-'.$id_user. '-' . $file->getClientOriginalName();
         $file->move(public_path() . $tmpFilePath, $tmpFileName);
         $path = $tmpFilePath . $tmpFileName;
@@ -136,7 +136,18 @@ class ContactController extends Controller
      */
     public function show($id)
     {
-        //
+        $properties = Property::all()->lists('nro','id');
+		$typecontacts = Typecontact::all()->lists('nombre','id');
+		$relationcontacts = Relationcontact::all()->lists('nombre','id');
+		$medias = Media::all()->lists('nombre','id');
+		$contact = Contact::find($id);
+		
+        return view('contacts.show')
+		->with('contact',$contact)
+		->with('properties',$properties)
+		->with('typecontacts',$typecontacts)
+		->with('relationcontacts',$relationcontacts)
+		->with('medias',$medias);
     }
 
     /**
@@ -147,7 +158,18 @@ class ContactController extends Controller
      */
     public function edit($id)
     {
-        //
+        $properties = Property::all()->lists('nro','id');
+		$typecontacts = Typecontact::all()->lists('nombre','id');
+		$relationcontacts = Relationcontact::all()->lists('nombre','id');
+		$medias = Media::all()->lists('nombre','id');
+		$contact = Contact::find($id);
+		
+        return view('contacts.edit')
+		->with('contact',$contact)
+		->with('properties',$properties)
+		->with('typecontacts',$typecontacts)
+		->with('relationcontacts',$relationcontacts)
+		->with('medias',$medias);
     }
 
     /**
@@ -159,7 +181,74 @@ class ContactController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request, [
+			'property' => 'required|not_in:0',
+			'typecontact' => 'required|not_in:0',
+			'relationcontact' => 'required|not_in:0',
+			'media' => 'required|not_in:0',
+			
+            'nombre' => 'required',
+            'apellido' => 'required',
+			'email' => 'required',
+			'correspondencia' => 'required',
+			'notas' => 'required'
+        ]);
+		
+		//Subir fotografia
+		if(!empty($request->fotografia)){
+			$id_user = Auth::user()->id;
+			$file = $request->file('fotografia');
+			$tmpFilePath = '/img/upload/fotografia_contacto/';
+			$tmpFileName = time() . '-'.$id_user. '-' . $file->getClientOriginalName();
+			$file->move(public_path() . $tmpFilePath, $tmpFileName);
+			$path = $tmpFilePath . $tmpFileName;
+		}
+
+
+        $company = Auth::user()->company;
+        
+		$activa = (empty($request->activa) ? '0' : $request->activa);
+		$mostrar_datos = (empty($request->mostrar_datos) ? '0' : $request->mostrar_datos);
+		$miembro_directorio = (empty($request->miembro_directorio) ? '0' : $request->miembro_directorio);
+		
+
+        $contact = Contact::find($id);
+        $contact->nombre = $request->nombre;
+        $contact->apellido = $request->apellido;
+		
+        $contact->telefono_movil = $request->telefono_movil;
+		$contact->telefono_domicilio = $request->telefono_domicilio;
+		$contact->telefono_oficina = $request->telefono_oficina;
+		
+		$contact->email = $request->email;
+		$contact->email_alterno = $request->email_alterno;
+		
+		$contact->direccion = $request->direccion;
+		$contact->profesion = $request->profesion;
+		$contact->nacionalidad = $request->nacionalidad;
+		
+        $contact->notas = $request->notas;
+		if(!empty($request->fotografia)){
+			$contact->fotografia = $path;
+		}
+
+		$contact->correspondencia = implode(",", $request->correspondencia);
+		
+		$contact->notas = $request->notas;
+		$contact->miembro_directorio = $miembro_directorio;
+		$contact->mostrar_datos = $mostrar_datos;
+        $contact->activa = $activa;
+		
+		$contact->property_id = $request->property;
+		$contact->typecontact_id = $request->typecontact;
+		$contact->relationcontact_id = $request->relationcontact;
+		$contact->media_id = $request->media;
+		
+        $contact->company_id = $company->id;
+
+        $contact->save();
+        Session::flash('message', 'Contacto actualizado exitosamente');
+        return redirect()->route('properties.contact.index');
     }
 
     /**
---
+++ resources/views/contacts/edit.blade.php
@@ -0,0 +1,293 @@
+@extends('layouts.admin')
+
+@section('admin-content')
+<div class="row wrapper border-bottom white-bg page-heading">
+    <div class="col-lg-10">
+        <h2>Contactos</h2>
+        <ol class="breadcrumb">
+            <li>
+                <a href="{{ route('admin.home') }}">Inicio</a>
+            </li>
+            <li>
+                <a href="{{ route('properties.contact.index') }}">Lista de contactos</a>
+            </li>
+            <li class="active">
+                <strong>Editar contacto</strong>
+            </li>
+        </ol>
+    </div>
+</div>
+
+<div class="wrapper wrapper-content animated fadeInRight">
+    <div class="row">
+        <div class="col-lg-12">
+
+            <div class="ibox float-e-margins">
+                <div class="ibox-content">
+					{!! Form::open(array('route' => array('properties.contact.update', $contact->id),'method' => 'patch' ,'class' => 'form-horizontal', 'files' => true)) !!}
+                    <div class="tabs-container">
+                        <ul class="nav nav-tabs">
+                            <li class="active"><a data-toggle="tab" href="#tab-1">Nuevo contacto</a></li>
+                        </ul>
+                        <div class="tab-content">
+                            <div id="tab-1" class="tab-pane active">
+                                <div class="panel-body">
+
+                                    <div class="form-group{{ $errors->has('property') ? ' has-error' : '' }}">
+                                        <label class="col-sm-3 control-label">Propiedad</label>
+                                        <div class="col-sm-3">
+                                            {{ Form::select('property',$properties, $contact->property_id, ['class' => 'form-control input-sm']) }}
+											@if ($errors->has('property'))
+												<span class="help-block">
+													<strong>{{ $errors->first('property') }}</strong>
+												</span>
+											@endif
+                                        </div>
+                                    </div>
+
+                                    <div class="hr-line-dashed"></div>
+
+                                    <div class="form-group{{ $errors->has('typecontact') ? ' has-error' : '' }}">
+                                        <label class="col-sm-3 control-label">Tipo</label>
+                                        <div class="col-sm-3">
+                                            {{ Form::select('typecontact',$typecontacts, $contact->typecontact_id, ['class' => 'form-control input-sm']) }}
+											@if ($errors->has('typecontact'))
+												<span class="help-block">
+													<strong>{{ $errors->first('typecontact') }}</strong>
+												</span>
+											@endif
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group{{ $errors->has('relationcontact') ? ' has-error' : '' }}">
+                                        <label class="col-sm-3 control-label">Relación</label>
+                                        <div class="col-sm-3">
+                                            {{ Form::select('relationcontact',$relationcontacts, $contact->relationcontact_id, ['class' => 'form-control input-sm']) }}
+											@if ($errors->has('relationcontact'))
+												<span class="help-block">
+													<strong>{{ $errors->first('relationcontact') }}</strong>
+												</span>
+											@endif
+                                        </div>
+                                    </div>
+
+                                    <div class="hr-line-dashed"></div>
+
+                                    <div class="form-group{{ $errors->has('nombre') ? ' has-error' : '' }}">
+                                        <label class="col-sm-3 control-label">Nombre</label>
+                                        <div class="col-sm-3">
+                                            <input type="text" class="form-control input-sm" name="nombre" value="{{$contact->nombre}}">
+											@if ($errors->has('nombre'))
+												<span class="help-block">
+													<strong>{{ $errors->first('nombre') }}</strong>
+												</span>
+											@endif
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group{{ $errors->has('apellido') ? ' has-error' : '' }}">
+                                        <label class="col-sm-3 control-label">Apellido</label>
+                                        <div class="col-sm-3">
+                                            <input type="text" class="form-control input-sm" name="apellido" value="{{$contact->apellido}}">
+											@if ($errors->has('apellido'))
+												<span class="help-block">
+													<strong>{{ $errors->first('apellido') }}</strong>
+												</span>
+											@endif
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label class="col-sm-3 control-label">Teléfonos</label>
+                                        <div class="col-sm-9">
+                                            <div class="row">
+                                                <div class="col-sm-4{{ $errors->has('telefono_movil') ? ' has-error' : '' }}">
+													<input type="text" placeholder="Móvil" class="form-control input-sm" name="telefono_movil" value="{{$contact->telefono_movil}}">
+													@if ($errors->has('telefono_movil'))
+													<span class="help-block">
+														<strong>{{ $errors->first('telefono_movil') }}</strong>
+													</span>
+													@endif
+												</div>
+                                                <div class="col-sm-4{{ $errors->has('telefono_domicilio') ? ' has-error' : '' }}">
+													<input type="text" placeholder="Domicilio" class="form-control input-sm" name="telefono_domicilio" value="{{$contact->telefono_domicilio}}">
+													@if ($errors->has('telefono_domicilio'))
+													<span class="help-block">
+														<strong>{{ $errors->first('telefono_domicilio') }}</strong>
+													</span>
+													@endif
+												</div>
+                                                <div class="col-sm-4{{ $errors->has('telefono_oficina') ? ' has-error' : '' }}">
+													<input type="text" placeholder="Oficina" class="form-control input-sm" name="telefono_oficina" value="{{$contact->telefono_oficina}}">
+													@if ($errors->has('telefono_oficina'))
+													<span class="help-block">
+														<strong>{{ $errors->first('telefono_oficina') }}</strong>
+													</span>
+													@endif
+												</div>
+												
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label class="col-sm-3 control-label">E-mail</label>
+                                        <div class="col-sm-9">
+                                            <div class="row">
+                                                <div class="col-sm-6{{ $errors->has('email') ? ' has-error' : '' }}">
+													<input type="text" placeholder="E-mail" class="form-control input-sm" name="email" value="{{$contact->email}}">
+													@if ($errors->has('email'))
+													<span class="help-block">
+														<strong>{{ $errors->first('email') }}</strong>
+													</span>
+													@endif
+												</div>
+                                                <div class="col-sm-6"><input type="text" placeholder="E-mail alterno" class="form-control input-sm" name="email_alterno" value="{{$contact->email_alterno}}"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label class="col-sm-3 control-label">Dirección</label>
+                                        <div class="col-sm-9">
+                                            <input type="text" class="form-control input-sm" name="direccion" value="{{$contact->direccion}}">
+                                        </div>
+                                    </div>
+
+                                    <div class="hr-line-dashed"></div>
+
+                                    <div class="form-group{{ $errors->has('fotografia') ? ' has-error' : '' }}">
+                                        <label class="col-sm-3 control-label">Fotografía</label>
+                                        <div class="col-sm-8">
+                                            <label title="Upload image file" for="inputImage" class="btn btn-white">
+                                                {{Form::file('fotografia', array('class'=>'') )}}
+												@if ($errors->has('fotografia'))
+													<span class="help-block">
+														<strong>{{ $errors->first('fotografia') }}</strong>
+													</span>
+												@endif
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="hr-line-dashed"></div>
+
+                                    <div class="form-group">
+                                        <label class="col-sm-3 control-label">Profesión</label>
+                                        <div class="col-sm-3">
+                                            <input type="text" class="form-control input-sm" name="profesion" value="{{$contact->profesion}}">
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label class="col-sm-3 control-label">Nacionalidad</label>
+                                        <div class="col-sm-3">
+                                            <input type="text" class="form-control input-sm" name="nacionalidad" value="{{$contact->nacionalidad}}">
+                                        </div>
+                                    </div>
+
+                                    <div class="hr-line-dashed"></div>
+
+                                    <div class="form-group{{ $errors->has('correspondencia') ? ' has-error' : '' }}">
+                                        <label class="col-sm-3 control-label">Correspondencia</label>
+                                        <div class="col-sm-3">
+                                            <label class="checkbox-inline">
+                                                <input type="checkbox" class="i-checks" name='correspondencia[]' value="Comunicados" {{ (in_array('Comunicados',explode(',',$contact->correspondencia))) ? 'checked' : '' }}>Comunicados</label>
+                                        </div>
+                                        <div class="col-sm-3">
+                                            <label class="checkbox-inline">
+                                                <input type="checkbox" class="i-checks" name='correspondencia[]' value="Cobranzas" {{ (in_array('Cobranzas',explode(',',$contact->correspondencia))) ? 'checked' : '' }}>Cobranzas</label>
+                                        </div>
+                                        <div class="col-sm-3">
+                                            <label class="checkbox-inline">
+                                                <input type="checkbox" class="i-checks" name='correspondencia[]' value="Directorio" {{ (in_array('Directorio',explode(',',$contact->correspondencia))) ? 'checked' : '' }}>Directorio</label>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group{{ $errors->has('media') ? ' has-error' : '' }}">
+                                        <label class="col-sm-3 control-label">Medio preferido</label>
+                                        <div class="col-sm-3">
+                                            {{ Form::select('media',$medias, $contact->media_id, ['class' => 'form-control input-sm']) }}
+											@if ($errors->has('media'))
+												<span class="help-block">
+													<strong>{{ $errors->first('media') }}</strong>
+												</span>
+											@endif
+                                        </div>
+                                    </div>
+
+                                    <div class="hr-line-dashed"></div>
+
+                                    <div class="form-group">
+                                        <label class="col-sm-3 control-label">Miembro Directorio</label>
+                                        <div class="col-sm-4">
+                                            <label><input type="checkbox" class="i-checks" name="miembro_directorio" value="1" {{ ($contact->miembro_directorio == 1) ? 'checked' : '' }}></label>
+                                        </div>
+                                    </div>
+
+                                    <div class="hr-line-dashed"></div>
+                                    
+                                    <div class="form-group">
+                                        <label class="col-sm-3 control-label">Mostrar mis datos</label>
+                                        <div class="col-sm-1">
+                                            <label><input type="checkbox" class="i-checks" name="mostrar_datos" value="1" {{ ($contact->mostrar_datos == 1) ? 'checked' : '' }}></label>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <p style="font-size: 11px; color: #B0B0B0;">Se refiere a que los datos de contacto son visibles para otros miembros.</p>
+                                        </div>
+                                    </div>
+
+                                    <div class="hr-line-dashed"></div>
+
+                                    <div class="form-group{{ $errors->has('notas') ? ' has-error' : '' }}">
+                                        <label class="col-sm-3 control-label">Notas</label>
+                                        <div class="col-sm-9">
+                                            <textarea rows="4" class="form-control input-sm" name="notas">{{ $contact->notas }}</textarea>
+											@if ($errors->has('notas'))
+												<span class="help-block">
+													<strong>{{ $errors->first('notas') }}</strong>
+												</span>
+											@endif
+										</div>
+                                    </div>
+
+                                    <div class="hr-line-dashed"></div>
+
+                                    <div class="form-group">
+                                        <label class="col-sm-3 control-label">Activa</label>
+                                        <div class="col-sm-4">
+                                            <input type="checkbox" class="i-checks" name="activa" value="1" {{ ($contact->activa == 1) ? 'checked' : '' }}>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="hr-line-dashed"></div>
+                        <div class="form-group">
+                            <div class="col-sm-12">
+                                <button class="btn btn-success" type="submit">Guardar</button>
+                                <a href="{{ route('properties.contact.index') }}" class="btn btn-white" >Cancelar</a>
+                            </div>
+                        </div>
+
+                    </div>
+                  {!! Form::close() !!}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
+@section('javascript')
+    <script>
+        $(document).ready(function () {
+            $('.i-checks').iCheck({
+                checkboxClass: 'icheckbox_square-green',
+                radioClass: 'iradio_square-green',
+            });
+
+        });
+    </script>
+@endsection
