c6cc7c04cdf5a06b4698064c37fa10d8e91fcd9c
Marcelo Choque <casiprogramador@gmail.com>
2017-05-11 23:44:36 -0400

    Reporte de estado de resultados aumento de anteriores y completado de filtros

--- resources/views/reports/estadoresultados_double_show.blade.php
+++ resources/views/reports/estadoresultados_double_show.blade.php
@@ -50,9 +50,17 @@
                         <div class="table-responsive" style="margin-top: 20px;">
                             <table class="table table-hover table-striped">
                                 <thead>
+									<tr bgcolor="#D6D6D6">
+                                        <th></th>
+                                        <th style="text-align:right;" colspan="2">ANTERIOR</th>
+                                        <th style="text-align:right;" colspan="2">ACTUAL</th>
+
+                                    </tr>
                                     <tr bgcolor="#D6D6D6">
                                         <th>Ingresos</th>
                                         <th style="text-align:right;">Porcentaje</th>
+                                        <th style="text-align:right;">Importe</th>
+										<th style="text-align:right;">Porcentaje</th>
                                         <th style="text-align:right;">Importe</th>
                                     </tr>
                                 </thead>
@@ -61,15 +69,30 @@
 									@for ($i = 0; $i < count($categorias_ingreso); $i++)
                                     <tr>
                                         <td>{{$categorias_ingreso[$i]['nombre']}}</td>
+										@if($anterior_importe_total_ingreso != 0)
+                                        <td style="text-align:right;">{{number_format($anterior_categorias_ingreso[$i]['monto']/$anterior_importe_total_ingreso*100,2)."%"}}</td>
+                                        @else
+                                        <td style="text-align:right;">{{"0%"}}</td>
+                                        @endif
+
+                                        <td style="text-align:right;">{{money_format('%i', $anterior_categorias_ingreso[$i]['monto'])}}</td>
+										
+										@if($importe_total_ingreso != 0)
                                         <td style="text-align:right;">{{number_format($categorias_ingreso[$i]['monto']/$importe_total_ingreso*100,2)."%"}}</td>
+                                        @else
+                                        <td style="text-align:right;">{{"0%"}}</td>
+                                        @endif
+										
                                         <td style="text-align:right;">{{money_format('%i', $categorias_ingreso[$i]['monto'])}}</td>
                                     </tr>
 									@endfor
                                 </tbody>
                                 <tfoot>
                                     <th>Total</th>
+									<th style="text-align:right;">100%</th>
+                                    <th style="text-align:right;">{{$anterior_importe_total_ingreso}}</th>
                                     <th style="text-align:right;">100%</th>
-                                    <th style="text-align:right;">{{money_format('%i', $importe_total_ingreso)}}</th>
+                                    <th style="text-align:right;">{{$importe_total_ingreso}}</th>
                                 </tfoot>
                             </table>
                         </div>
@@ -81,6 +104,8 @@
                                 <thead>
                                     <tr bgcolor="#D6D6D6">
                                         <th>Gastos</th>
+										<th style="text-align:right;">Porcentaje</th>
+                                        <th style="text-align:right;">Importe</th>
                                         <th style="text-align:right;">Porcentaje</th>
                                         <th style="text-align:right;">Importe</th>
                                     </tr>
@@ -88,28 +113,52 @@
 
                                 <tbody>
                                     <tr>
-                                        <td colspan="3"><strong>Fijos</strong></td>
+                                        <td colspan="5"><strong>Fijos</strong></td>
                                     </tr>
                                     @for ($i = 0; $i < count($categorias_egreso_ordinario); $i++)
                                     <tr>
                                         <td>{{$categorias_egreso_ordinario[$i]['nombre']}}</td>
+										@if($anterior_importe_total_egreso != 0)
+                                        <td style="text-align:right;">{{number_format($anterior_categorias_egreso_ordinario[$i]['monto']/$anterior_importe_total_egreso*100,2)."%"}}</td>
+                                        @else
+                                        <td style="text-align:right;">{{"0%"}}</td>
+                                        @endif
+                                        <td style="text-align:right;">{{$anterior_categorias_egreso_ordinario[$i]['monto']}}</td>
+										
+                                        @if($importe_total_egreso != 0)
                                         <td style="text-align:right;">{{number_format($categorias_egreso_ordinario[$i]['monto']/$importe_total_egreso*100,2)."%"}}</td>
-                                        <td style="text-align:right;">{{money_format('%i', $categorias_egreso_ordinario[$i]['monto'])}}</td>
+                                        @else
+                                        <td style="text-align:right;">{{"0%"}}</td>
+                                        @endif
+                                        <td style="text-align:right;">{{$categorias_egreso_ordinario[$i]['monto']}}</td>
                                     </tr>
 									@endfor
                                     <tr>
-                                        <td colspan="3" style="background-color: #E7E7E7;"><b>Variables</b></td>
+                                        <td colspan="5" style="background-color: #E7E7E7;"><b>Variables</b></td>
                                     </tr>
                                     @for ($i = 0; $i < count($categorias_egreso_extraordinario); $i++)
                                     <tr>
                                         <td>{{$categorias_egreso_extraordinario[$i]['nombre']}}</td>
+										@if($anterior_importe_total_egreso != 0)
+                                        <td style="text-align:right;">{{number_format($anterior_categorias_egreso_extraordinario[$i]['monto']/$anterior_importe_total_egreso*100,2)."%"}}</td>
+                                        @else
+                                        <td style="text-align:right;">{{"0%"}}</td>
+                                        @endif
+                                        <td style="text-align:right;">{{$anterior_categorias_egreso_extraordinario[$i]['monto']}}</td>
+										
+                                        @if($importe_total_egreso != 0)
                                         <td style="text-align:right;">{{number_format($categorias_egreso_extraordinario[$i]['monto']/$importe_total_egreso*100,2)."%"}}</td>
-                                        <td style="text-align:right;">{{money_format('%i', $categorias_egreso_extraordinario[$i]['monto'])}}</td>
+                                        @else
+                                        <td style="text-align:right;">{{"0%"}}</td>
+                                        @endif
+                                        <td style="text-align:right;">{{$categorias_egreso_extraordinario[$i]['monto']}}</td>
                                     </tr>
 									@endfor
                                 </tbody>
                                 <tfoot>
                                     <th>Total</th>
+									<th style="text-align:right;">100.00%</th>
+                                    <th style="text-align:right;">{{$anterior_importe_total_egreso}}</th>
                                     <th style="text-align:right;">100.00%</th>
                                     <th style="text-align:right;">{{$importe_total_egreso}}</th>
                                 </tfoot>
@@ -124,35 +173,41 @@
                                         <th>Resultado</th>
                                         <th style="text-align:right;"></th>
                                         <th style="text-align:right;">Importe</th>
+										<th style="text-align:right;"></th>
+                                        <th style="text-align:right;">Importe</th>
                                     </tr>
                                 </thead>
 
                                 <tbody>
                                     <tr>
                                         <td>Ingresos</td>
+										<td style="text-align:right;"></td>
+                                        <td style="text-align:right;">{{$anterior_importe_total_ingreso}}</td>
                                         <td style="text-align:right;"></td>
-                                        <td style="text-align:right;">{{money_format('%i', $importe_total_ingreso)}}</td>
+                                        <td style="text-align:right;">{{$importe_total_ingreso}}</td>
                                     </tr>
                                     <tr>
                                         <td>Gastos fijos</td>
+										<td style="text-align:right;"></td>
+                                        <td style="text-align:right;">{{$anterior_importe_total_egreso_ordinario}}</td>
                                         <td style="text-align:right;"></td>
-                                        <td style="text-align:right;">{{money_format('%i', $importe_total_egreso_ordinario)}}</td>
+                                        <td style="text-align:right;">{{$importe_total_egreso_ordinario}}</td>
                                     </tr>
                                     <tr>
                                         <td>Gastos variables</td>
+										<td style="text-align:right;"></td>
+                                        <td style="text-align:right;">{{$anterior_importe_total_egreso_extraordinario}}</td>
                                         <td style="text-align:right;"></td>
-                                        <td style="text-align:right;">{{money_format('%i', $importe_total_egreso_extraordinario)}}</td>
+                                        <td style="text-align:right;">{{$importe_total_egreso_extraordinario}}</td>
                                     </tr>
                                     <tr>
                                         <td>Diferencia del periodo</td>
+										<td style="text-align:right;"></td>
+
+                                        <td style="text-align:right; color: #0E9AEF">{{$anterior_importe_total_ingreso-$anterior_importe_total_egreso}}</td>
                                         <td style="text-align:right;"></td>
 
-                                        <!-- Valor mayor o igual a cero (0) -->
-                                        <td style="text-align:right; color: #0E9AEF">{{money_format('%i', $importe_total_ingreso-$importe_total_egreso)}}</td>
-
-                                        <!-- Valor mayor o igual a cero (0)
-                                        <td style="text-align:right; color: red">670,00</td>
-                                        -->
+                                        <td style="text-align:right; color: #0E9AEF">{{$importe_total_ingreso-$importe_total_egreso}}</td>
 
                                     </tr>
                                 </tbody>
@@ -169,7 +224,7 @@
                             <div class="hr-line-dashed"></div>
 
                             <div class="form-group text-left">
-                                <button class="btn btn-success" type="submit">Volver</button>
+                                <a href="{{ route('report.estadoresultados') }}" class="btn btn-success" type="submit">Volver</a>
                             </div>
                         </div>
                     </div>
